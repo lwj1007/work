284687e4329f3f9247c8faf1499b9714c51224cb
lwj <2321037461@qq.com>
2020-04-03 10:54:17 +0800

    补充

---
+++ IPersistence/Arguments.java
@@ -0,0 +1,8 @@
+package com.lagou.pojo;
+
+public class Arguments {
+    public static String SELECT="select";
+    public static String ADD="insert";
+    public static String DELETE="delete";
+    public static String UPDATE="update";
+}
